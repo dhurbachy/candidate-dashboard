d22aeedbd9ad8300ed24c6276dab6849da9d0def
Dhurba Chaudhary <dhurbac66@gmail.com>
2026-07-04 23:46:29 +0545

    chores: misused of bind

--- backend/app/database.py
+++ backend/app/database.py
@@ -10,7 +10,7 @@ engine=create_engine(
     echo=False
 )
 
-SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=create_engine)
+SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
 Base=declarative_base()
 
