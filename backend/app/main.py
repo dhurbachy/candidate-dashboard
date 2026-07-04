a0e54b643c1fd4a497c8b3036f492318b133d20e
Dhurba Chaudhary <dhurbac66@gmail.com>
2026-07-04 20:38:03 +0545

    chores:add app lifespan configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+
+    yield
+    
 app=FastAPI(title="Candidate Dashboard")
 
 @app.get("/health")
