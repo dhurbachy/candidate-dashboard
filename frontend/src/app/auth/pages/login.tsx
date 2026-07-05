433e026774b6e7e9bf549d90e8faf545bc7cdda7
Dhurba Chaudhary <dhurbac66@gmail.com>
2026-07-05 08:13:50 +0545

    chores:add login ui

--- frontend/src/app/auth/pages/login.tsx
+++ frontend/src/app/auth/pages/login.tsx
@@ -1,7 +1,13 @@
+import { LoginForm } from "../../../components/login-form"
+// import { Navigate } from "react-router";
 export default function Login(){
     return(
         <>
-        Login
+         <div className="min-h-screen bg-[#feffe6] flex items-center justify-center px-4">
+            <div className="w-full max-w-sm">
+                <LoginForm />
+            </div>
+        </div>
         </>
     )
 }
